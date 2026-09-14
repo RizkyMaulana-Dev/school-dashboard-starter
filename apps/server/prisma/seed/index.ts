7237e957355280b5805f18b3d8d3f39b464ad82b
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2026-09-14 19:42:16 +0700

    fix(server): add nodejs_compat, lock wrangler version, and replace bcrypt with bcryptjs

--- apps/server/prisma/seed/index.ts
+++ apps/server/prisma/seed/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { logger } from "../../src/lib/logger";
 
 const prisma = new PrismaClient();
--- apps/server/src/modules/users/user.service.ts
+++ apps/server/src/modules/users/user.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import { UserRepository } from "./user.repository.js";
 import { CreateUserDto, UpdateUserDto } from "./user.types.js";
